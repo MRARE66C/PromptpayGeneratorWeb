7f4ce74b3ab1ce97b7d7c2d7ae8bd41ccf370f84
MRARE66C <tvsupegame@gmail.com>
2025-05-14 13:45:23 +0700

    frontend update

--- promptpaygenerator-web/src/App.jsx
+++ promptpaygenerator-web/src/App.jsx
@@ -1,30 +1,42 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import promptpayLogo from './assets/icon-thaiqr.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [promptpayId, setPromptpayId] = useState('');
+  const [Amount, setAmount] = useState('');
+  const [genrate, setgenerate] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://www.bot.or.th/th/financial-innovation/digital-finance/digital-payment/promptpay.html" target="_blank">
+          <img src={promptpayLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Promptpay Generator</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <h3>Enter Promptpay ID</h3>
+        <input
+          type="text"
+          placeholder="Phone number or National ID"
+          value={promptpayId}
+          onChange={(e) => setPromptpayId(e.target.value)}
+          className='Input'
+        />
+        <h3>Amount (THB)</h3>
+        <input
+          type="text"
+          placeholder="THB"
+          value={Amount}
+          onChange={(e) => setAmount(e.target.value)}
+          className='Input'
+        />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button>
+        Generate
+      </button>
     </>
   )
 }
